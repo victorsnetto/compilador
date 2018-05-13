51c0dee561c21f9d49a78f4491d2015b51edaa4d
Flávio Ghiraldeli <ghiraldeli26@gmail.com>
2018-05-13 18:18:32 -0300

    Base Compilador

--- Compilador/src/main/Main.java
+++ Compilador/src/main/Main.java
@@ -1,10 +1,16 @@
 package main;
 
+import java.io.IOException;
+
+import logica.Compilador;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-
+		String caminho = "caminho/do/arquivo/fonte";
+		Compilador compilador = new Compilador(caminho);
+		compilador.compilar();
 	}
 
 }
