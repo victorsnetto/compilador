88e786520d9221114d9b67ed72ea03349d3f3323
Flávio Ghiraldeli <ghiraldeli26@gmail.com>
2018-05-13 17:48:27 -0300

    Base Compilador

---
+++ Compilador/src/logica/Compilador.java
@@ -0,0 +1,35 @@
+package logica;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Compilador {
+	
+	private final String arquivo;
+	
+	public Compilador(String arquivo)
+	{
+		this.arquivo = arquivo;
+	}
+	
+	public void compilar() throws IOException 
+	{
+		FileInputStream stream = new FileInputStream(arquivo);
+		InputStreamReader reader = new InputStreamReader(stream);
+		BufferedReader br = new BufferedReader(reader);
+		String linha = br.readLine();
+		String novaLinha = "";
+		
+		while(linha != null)
+		{
+			novaLinha = linha;
+			System.out.println(novaLinha);
+			linha = br.readLine();
+		}
+		
+		br.close();
+		
+	}
+}
